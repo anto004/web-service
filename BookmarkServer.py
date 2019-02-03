a9d66b38d8a115368b3e196b9c1a62acbf306b45
anto004 <anto_004@live.com>
2019-02-02 17:16:58 -0800

    Add Bookmark server

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -2,6 +2,7 @@
 #
 import http.server
 import requests
+import os
 from urllib.parse import unquote, parse_qs
 
 memory = {}
@@ -103,6 +104,22 @@ class Shortener(http.server.BaseHTTPRequestHandler):
 
 
 if __name__ == '__main__':
-    server_address = ('', 8000)
+    port = int(os.environ.get('PORT', 8000))
+    server_address = ('', port)
     httpd = http.server.HTTPServer(server_address, Shortener)
     httpd.serve_forever()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # web-service
+install Heroku client [Heroku cli](https://devcenter.heroku.com/articles/heroku-cli)
+   brew install heroku/brew/heroku
